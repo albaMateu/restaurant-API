154675e6f5da4701627dad87efd05a3d66445cdf
unknown <alba.alm.5@gmail.com>
2020-08-26 11:15:47 +0200

    sales

--- functions/sales.php
+++ functions/sales.php
@@ -8,9 +8,26 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 function getSales (Request $request, Response $response){
     $sql= "SELECT * FROM sala ORDER BY id DESC;";
     $db= getConnection();
-    $query= $db->query($sql);
+    $stmt= $db->prepare($sql);
+    /* $stmt->bind_param(); */
+    $stmt->execute(); 
+    $query=$stmt->get_result();
+    /*$query=$db->query($sql); */
 
-    error_log(date("d-m-Y H:i:s"). $db->errno. ":".$db->error."\n", 3, "C:/xampp/htdocs/Restaurant-API/logs/errors.log");
+    var_dump($stmt->sqlstate);
+
+    if (!$query) {
+        echo "Falló la ejecución: (" . $stmt->errno . ") " . $stmt->error;        
+        error_log(date("d-m-Y H:i:s"). $stmt->errno. ":".$stmt->error."\n", 3, "C:/xampp/htdocs/Restaurant-API/logs/errors.log");
+        
+        $result = array(
+            'status'=>'error',
+            'code' => $stmt->errno,
+            'message' => $stmt->error
+        );
+        $db= null;
+        return $result;
+    }
     $db=null;
 
     //crear una array de obejtos a partir del resultado de la query
