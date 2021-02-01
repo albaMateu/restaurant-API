95dceb7a9d5ba8ccb8a59104c90ebaa37696b038
albaMateu <alba.alm.5@gmail.com>
2021-02-01 17:21:47 +0100

    routes

--- src/App/Routes.php
+++ src/App/Routes.php
@@ -2,6 +2,11 @@
 
 use Slim\Routing\RouteCollectorProxy;
 
-$app->group('/v1'. function(RouteCollectorProxy $group){
+$app->group('/v1', function(RouteCollectorProxy $group){
     $group ->get('/sales','App\Controllers\SalesController:getAll');
 });
+
+
+/* $app->group('/v1'. function(RouteCollectorProxy $group){
+    $group ->get('/sales','App\Controllers\SalesController:getAll');
+}); */
