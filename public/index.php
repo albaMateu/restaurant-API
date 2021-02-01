7497762ce3f91969ac783fbf1fe1b00fa70f0611
albaMateu <alba.alm.5@gmail.com>
2021-02-01 12:35:58 +0100

    index

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ use Slim\Factory\AppFactory;
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
-$app->setBasePath("/restaurant-api/public");
+$app->setBasePath("/restaurant-api"); // IMPRESCINDIBLE el que volem que siga la URL base del projecte (en este cas hem omitit la carpeta public)
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
