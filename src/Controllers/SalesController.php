8cddf54c76ce789f700c6b4817d8dc4be43a2184
albaMateu <alba.alm.5@gmail.com>
2021-02-03 18:26:00 +0100

    sales

--- src/Controllers/SalesController.php
+++ src/Controllers/SalesController.php
@@ -10,7 +10,7 @@ class SalesController extends BaseController{
     public function getAll($resquest, $response, $arg){
         //porta les dades del contenedor que porta la connexió a BD
         $pdo=$this->container->get('db');
-        $query=$pdo->query("SELECT* FROM sales;");
+        $query=$pdo->query("SELECT * FROM sales;");
         $response->getBody()->write(json_encode($query->fetchAll()));
         return $response
             ->withHeader('Conten-Type','application/json')
