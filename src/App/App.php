ea66521e8ce1efd055729c0c36a81f6803843f30
albaMateu <alba.alm.5@gmail.com>
2021-10-12 20:41:22 +0200

    basepath server

--- src/App/App.php
+++ src/App/App.php
@@ -4,21 +4,24 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../../vendor/autoload.php';
 
-$container_aux= new \DI\Container();
+$container_aux = new \DI\Container();
 AppFactory::setContainer($container_aux);
 
 $app = AppFactory::create();
-$app->setBasePath("/restaurant-api"); // IMPRESCINDIBLE el que volem que siga la URL base del projecte (en este cas hem omitit la carpeta public)
+/* localhost */
+/* $app->setBasePath("/restaurant-api"); */ // IMPRESCINDIBLE el que volem que siga la URL base del projecte (en este cas hem omitit la carpeta public)
+/* server */
+$app->setBasePath("/");
 
 /* $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
     return $response;
 }); */
 
-$container=$app->getContainer();
-require __DIR__."/Routes.php";
-require __DIR__."/Config.php";
-require __DIR__."/Dependencies.php";
-require __DIR__."/Utilities.php";
+$container = $app->getContainer();
+require __DIR__ . "/Routes.php";
+require __DIR__ . "/Config.php";
+require __DIR__ . "/Dependencies.php";
+require __DIR__ . "/Utilities.php";
 
 $app->run();
