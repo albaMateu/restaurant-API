34aa795c5991278e69fc9490e3418a1fabd7c087
albaMateu <alba.alm.5@gmail.com>
2022-11-27 19:43:18 +0100

    registro de clientes

--- src/Controllers/UsersController.php
+++ src/Controllers/UsersController.php
@@ -60,16 +60,20 @@ class UsersController extends BaseController
 
     public function register($request, $response)
     {
-        $json = file_get_contents('php://input');
+        $hash = file_get_contents('php://input');
 
-        $hash = json_decode($json, true);
-        /* $user = explode(":", $hash);
+        $hash = base64_decode($hash);
+        $user = explode(":", $hash);
 
-        $email= $user[0];
-        $pwd= $user[1]; */
+        $email = $user[0];
+        $pwd = $user[1];
+
+
+        $code = UsersController::insertUser($email, $pwd);
 
         $result = array(
-            "token" => $hash
+            "token" => $user[2],
+            "code" => $code
         );
 
 
@@ -81,46 +85,26 @@ class UsersController extends BaseController
 
 
 
-    //insertar usuari a la base de dades
-    public function insertUser($resquest, $response)
+    //insertar client a la base de dades
+    public function insertUser($email, $pwd)
     {
-        $json = file_get_contents('php://input');
-
-        $post = json_decode($json, true);
-
-        $pers = htmlspecialchars($post['pers']);
-        $dia = $post['dia'];
-        $hora = $post['hora'];
-        $taules = $post['taules'];
-        $sala = $post['sala'];
-        $nom = htmlspecialchars($post['nom']);
-        $tel = $post['tel'];
-        $email = htmlspecialchars($post['email']);
-        if (isset($post['coment'])) {
-            $coment = htmlspecialchars($post['coment']);
-        } else {
-            $coment = "";
-        }
+        $today = date('UTC+1');
 
         //porta les dades del contenedor que porta la connexió a BD
         $pdo = $this->container->get('db');
-        $sql = "INSERT INTO reserves (pers, dia, hora, taules, sala, nom, tel, email, coment) 
-        VALUES (:pers, :dia, :hora, :taules, :sala, :nom, :tel, :email, :coment);";
+        $sql = "INSERT INTO usuaris (email, pwd, actiu, rol) 
+        VALUES ( :email, :pwd, 1, 3);";
 
 
         try {
             $query = $pdo->prepare($sql);
-            $query->bindParam(':pers', $pers);
-            $query->bindParam(':dia', $dia);
-            $query->bindParam(':hora', $hora);
-            $query->bindParam(':taules', $taules);
-            $query->bindParam(':sala', $sala);
-            $query->bindParam(':nom', $nom);
-            $query->bindParam(':tel', $tel);
+            /*  $query->bindParam(':nom', 'alba');
+            $query->bindParam(':cognoms', 'algo '); */
             $query->bindParam(':email', $email);
-            $query->bindParam(':coment', $coment);
-            /* $result=$query->execute(); */
-            $result = true;
+            $query->bindParam(':pwd', $pwd);
+            /* $query->bindParam(':tel', '666666666'); */
+            $result = $query->execute();
+            /* $result = true; */
 
             if ($result) {
                 $code = 200;
@@ -132,15 +116,7 @@ class UsersController extends BaseController
             utilities::logError($code, "Falló la ejecución: (" . $err->getMessage() . ") " . $err->getCode());
         }
 
-        //devuelve el array
-
-        $result = array(
-            "code" => $code
-        );
-
-        //el encode es precis ahi, sino no va
-        $response->getBody()->write(json_encode($result));
-        return $response;
+        return $code;
     }
 
 
@@ -199,8 +175,7 @@ class UsersController extends BaseController
 
 
         $result = array(
-            "exist" => $exist,
-            "mail" => $email
+            "exist" => $exist
         );
 
         //transforma array a json
